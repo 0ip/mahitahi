4258150247dca7ae518ef945269e57066edbb13a
0ip <me@factor.cc>
2017-11-13 12:52:14 +0100

    Fix allocation test bug

--- tests/test_alloc.py
+++ tests/test_alloc.py
@@ -7,12 +7,16 @@ from mahitahi.strategy import RandomStrategy
 BASE_BITS = 5
 
 
-@pytest.mark.parametrize("left_pos,left_sites,right_pos,right_sites,possible_pos", [
-    ([0], [-1], [0, 1], [-1, 0], ([0, 0, 5], [0, 0, 123]))
+@pytest.mark.parametrize("l_pos,l_sites,r_pos,r_sites,res_pos_u,res_pos_l,res_pos_depth", [
+    ([0], [-1], [0, 1], [-1, 0], 5, 123, 3)
 ])
-def test_alloc(left_pos, left_sites, right_pos, right_sites, possible_pos):
-    test_alloc = Allocator(RandomStrategy(), 0)
-    p1 = Position(left_pos, left_sites, base_bits=BASE_BITS)
-    p2 = Position(right_pos, right_sites, base_bits=BASE_BITS)
+def test_alloc_pos(l_pos, l_sites, r_pos, r_sites, res_pos_u, res_pos_l, res_pos_depth):
+    test_allocator = Allocator(RandomStrategy(), 0)
+    p1 = Position(l_pos, l_sites, base_bits=BASE_BITS)
+    p2 = Position(r_pos, r_sites, base_bits=BASE_BITS)
 
-    assert test_alloc(p1, p2).pos in possible_pos
+    int_at_depth = p1.interval_at(res_pos_depth)
+
+    res_pos = test_allocator(p1, p2).pos
+
+    assert 0 < res_pos[-1] <= res_pos_u or res_pos_l <= res_pos[-1] < int_at_depth
